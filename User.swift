9ccbdfc65f706ddfa6286a93446181ad1016ede5
hkncox <hkncox@gmail.com>
2016-01-28 10:58:01 -0700

    added email validation

--- User.swift
+++ User.swift
@@ -27,7 +27,15 @@ class User: UIViewController
     @IBOutlet var button_signUp: UIButton?
     @IBAction func buttonTapped(button_signUp: UIButton)
     {
-        validate(text_firstName.text: String, text_lastName.text: String, text_createUserName.text: <#T##String#>, text_confirmUserName.text: <#T##String#>, text_createPassword.text: String, text_confirmPassword.text: String, text_email.text: <#T##String#>)
+       /*  validate(text_firstName.text?, _lastName: text_lastName.text?, _createUserName: text_createUserName.text!, _confirmUserName: text_confirmUserName.text!, _createPassword: text_createPassword.text!, _confirmPassword: text_confirmPassword.text!, _email: text_email.text!)*/
+        if isValidEmail(text_email.text!)
+        {
+            text_email.text = "Success!"
+        }
+        else {
+         //   text_email.textColor
+            text_email.text = "Invalid email. Please try again."
+        }
     }
     func User(_firstName: String, _lastName: String, _userName: String, _password: String, _email: String)
     {
@@ -37,7 +45,8 @@ class User: UIViewController
         password = _password;
         email = _email;
     }
-    func validate(_firstName: String, _lastName: String, _createUserName: String, _confirmUserName: String, _createPassword: String, _confirmPassword: String, _email: String)
+ 
+    func validate(_firstName: String?, _lastName: String?, _createUserName: String?, _confirmUserName: String?, _createPassword: String?, _confirmPassword: String?, _email: String?)
     {
         if !(isValidEmail(text_email.text!))
         {
@@ -46,13 +55,17 @@ class User: UIViewController
         
         
     }
-    func isValidEmail(testStr:String) -> Bool {
+    func isValidEmail(email: String) -> Bool {
         
-        print("validate emilId: \(testStr)")
+        print("validate emilId: \(email)")
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        let result = emailTest.evaluateWithObject(testStr)
+        let result = emailTest.evaluateWithObject(email)
         return result
         
     }
+    @IBAction func dismissKeyboard(sender: AnyObject){
+        textField.resignFirstResponder()
+    }
+
 }
